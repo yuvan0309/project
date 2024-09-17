889e79ef412cba63e3fe915f94ca2d9c9e5b1d66
yuvan <yuvan7480@gmail.com>
2024-09-17 17:00:20 +0530

    sync

---
+++ python/day2.py
@@ -0,0 +1,8 @@
+def clg(profname,subknowledge,profdept="ece"):
+    print("profname:",profname)
+    print("subknowledge:",subknowledge)
+    print("profdept:",profdept)
+clg("govind",{"datastructures":"70%","maths":"30%"},"cse")
+clg("hari",{"datastructures":"30%","maths":"80%"})
+
+

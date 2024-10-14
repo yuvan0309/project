426f688694721a9ba5becea8e50df95ead72864a
yuvan <yuvan7480@gmail.com>
2024-10-14 23:50:20 +0530

    sync

--- java/CN/crc.java
+++ java/CN/crc.java
@@ -6,17 +6,17 @@ class crc {
         BufferedReader br = new BufferedReader(isr);
         int[] message;
         int[] gen;
-        int[] app_message;
+        int[] app_msg;
         int[] rem;
-        int[] trans_message;
-        int message_bits, gen_bits, total_bits;
+        int[] trans_msg;
+        int msg_bits, gen_bits, total_bits;
 
         System.out.println("\n Enter number of bits in message : ");
-        message_bits = Integer.parseInt(br.readLine());
-        message = new int[message_bits];
+        msg_bits = Integer.parseInt(br.readLine());
+        message = new int[msg_bits];
 
         System.out.println("\n Enter message bits : ");
-        for (int i = 0; i < message_bits; i++)
+        for (int i = 0; i < msg_bits; i++)
             message[i] = Integer.parseInt(br.readLine());
 
         System.out.println("\n Enter number of bits in gen : ");
@@ -27,16 +27,16 @@ class crc {
         for (int i = 0; i < gen_bits; i++)
             gen[i] = Integer.parseInt(br.readLine());
 
-        total_bits = message_bits + gen_bits - 1;
-        app_message = new int[total_bits];
+        total_bits = msg_bits + gen_bits - 1;
+        app_msg = new int[total_bits];
         rem = new int[total_bits];
-        trans_message = new int[total_bits];
+        trans_msg = new int[total_bits];
 
         for (int i = 0; i < message.length; i++)
-            app_message[i] = message[i];
+            app_msg[i] = message[i];
 
         System.out.print("\n Message bits are : ");
-        for (int i = 0; i < message_bits; i++)
+        for (int i = 0; i < msg_bits; i++)
             System.out.print(message[i]);
 
         System.out.print("\n Generators bits are : ");
@@ -44,33 +44,33 @@ class crc {
             System.out.print(gen[i]);
 
         System.out.print("\n Appended message is : ");
-        for (int i = 0; i < app_message.length; i++)
-            System.out.print(app_message[i]);
+        for (int i = 0; i < app_msg.length; i++)
+            System.out.print(app_msg[i]);
 
-        for (int j = 0; j < app_message.length; j++)
-            rem[j] = app_message[j];
+        for (int j = 0; j < app_msg.length; j++)
+            rem[j] = app_msg[j];
 
-        rem = computecrc(app_message, gen, rem);
+        rem = computecrc(app_msg, gen, rem);
 
-        for (int i = 0; i < app_message.length; i++)
-            trans_message[i] = (app_message[i] ^ rem[i]);
+        for (int i = 0; i < app_msg.length; i++)
+            trans_msg[i] = (app_msg[i] ^ rem[i]);
 
         System.out.println("\n Transmitted message from the transmitter is : ");
-        for (int i = 0; i < trans_message.length; i++)
-            System.out.print(trans_message[i]);
+        for (int i = 0; i < trans_msg.length; i++)
+            System.out.print(trans_msg[i]);
 
         System.out.println("\n Enter received message of " + total_bits + " bits at receiver end : ");
-        for (int i = 0; i < trans_message.length; i++)
-            trans_message[i] = Integer.parseInt(br.readLine());
+        for (int i = 0; i < trans_msg.length; i++)
+            trans_msg[i] = Integer.parseInt(br.readLine());
 
         System.out.println("\n Received message is :");
-        for (int i = 0; i < trans_message.length; i++)
-            System.out.print(trans_message[i]);
+        for (int i = 0; i < trans_msg.length; i++)
+            System.out.print(trans_msg[i]);
 
-        for (int j = 0; j < trans_message.length; j++)
-            rem[j] = trans_message[j];
+        for (int j = 0; j < trans_msg.length; j++)
+            rem[j] = trans_msg[j];
 
-        rem = computecrc(trans_message, gen, rem);
+        rem = computecrc(trans_msg, gen, rem);
 
         for (int i = 0; i < rem.length; i++) {
             if (rem[i] != 0) {
@@ -82,7 +82,7 @@ class crc {
         }
     }
 
-    static int[] computecrc(int app_message[], int gen[], int rem[]) {
+    static int[] computecrc(int app_msg[], int gen[], int rem[]) {
         int current = 0;
         while (true) {
             for (int i = 0; i < gen.length; i++)

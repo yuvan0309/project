d9338f07e15b283bdeac1266a10f236b0cae32ec
yuvan0309 <yuvan7480@gmail.com>
2025-02-24 23:43:19 +0530

    asdw

--- ASM/add.asm
+++ ASM/add.asm
@@ -6,8 +6,5 @@
 
 XS b XS
 	END
-spim -file add.asm
 // Output:
 // SPIM Version 9.1.20 of August 1, 2013
-// The SPIM S20 / S21 / S22 architecture is Copyright 1990-2013 by James Larus.
-// All Rights Reserved.
